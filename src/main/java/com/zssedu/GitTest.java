637d6c73e5aea6bc8a648263177c29e31db20702
Tommy <2030509072@qq.com>
2023-03-10 08:25:48 +0800

    hot-fix first commit

--- src/main/java/com/zssedu/GitTest.java
+++ src/main/java/com/zssedu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master first commit");
         System.out.println("master second commit");
+        System.out.println("hot-fix first commit");
     }
 }
